f00e929161c553c33f0e1c974c1c5676d5084130
Logan-Developer <logan2.humbert@gmail.com>
2022-10-24 16:23:12 +0200

    Manage list of connected users

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,19 @@
 "use strict";
 
+function updateConnectedUsersList(clients) {
+    var list = document.getElementById("content").getElementsByTagName("aside")[0];
+        list.innerHTML = "";
+
+        for (var i = 0; i < clients.length; i++) {
+            var p = document.createElement("p");
+            p.textContent = clients[i];
+
+            p.setAttribute("data-score", 0);
+
+            list.appendChild(p);
+        }
+}
+
 document.addEventListener("DOMContentLoaded", function(_e) {
 
     // socket opened to client
@@ -22,12 +36,26 @@ document.addEventListener("DOMContentLoaded", function(_e) {
         }
     });
 
+    // handle login errors
     sock.on("error-connection", function(msg) {
         alert(msg);
     });
 
+    
+    /**********************************************
+     * Connected users list
+     *********************************************/
+
+    // get list when we are connected, and display chat ui
     sock.on("welcome", function(clients) {
         document.getElementById("radio2").checked = true;
+
+        updateConnectedUsersList(clients);
+    });
+
+    // update list when a new user is connected
+    sock.on("list", function(clients) {
+        updateConnectedUsersList(clients);
     });
 });
     
