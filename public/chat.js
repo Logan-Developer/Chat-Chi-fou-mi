4a6f791a5d677e995fd72522567845c87b22f08b
Logan-Developer <logan2.humbert@gmail.com>
2022-10-24 15:58:33 +0200

    Handle client connection

--- public/chat.js
+++ public/chat.js
@@ -8,6 +8,26 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     // At start : force to display login form
     document.getElementById("radio1").checked = true;
     document.getElementById("pseudo").focus();
-    
+
+
+    /**********************************************
+     * Login
+     *********************************************/
+
+    // When the user clicks on the button "Login"
+    document.getElementById("btnConnect").addEventListener("click", function(e) {
+        var pseudo = document.getElementById("pseudo").value;
+        if (pseudo != "") {
+            sock.emit("login", pseudo);
+        }
+    });
+
+    sock.on("error-connection", function(msg) {
+        alert(msg);
+    });
+
+    sock.on("welcome", function(clients) {
+        document.getElementById("radio2").checked = true;
+    });
 });
     
